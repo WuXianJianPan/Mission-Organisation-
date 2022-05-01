7315a21c43ca5dde4719f5d2ea5c93c755cd0362
Yan <yantao@bupt.edu.cn>
2022-05-01 14:42:34 +0800

    build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import data from'./data.json'
+import Searching from './search.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="Wrapper">
+        <Searching />
+      </div>
+    );
+  }
+}
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
---
+++ src/search.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import './index.css';
+import Data from'./data.json'
+import { ConfigProvider, DatePicker, message , Input, Space , Descriptions , Table } from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/lib/locale/zh_CN';
+import 'antd/dist/antd.css';
+import './search.css'
+
+const data = Data['data']
+const { Search } = Input;
+
+const columns = [
+  {
+    title: 'Organisation Name',
+    dataIndex: 'Organisation Name',
+    key: 'Organisation Name',
+  },
+  {
+    title: 'Town/City',
+    dataIndex: 'Town/City',
+    key: 'Town/City',
+  },
+  {
+    title: 'County',
+    dataIndex: 'County',
+    key: 'County',
+  },
+  {
+    title: 'Type & Rating',
+    dataIndex: 'Type & Rating',
+    key: 'Type & Rating',
+  },
+  {
+    title: 'Route',
+    dataIndex: 'Route',
+    key: 'Route',
+  }
+];
+
+function Item(props){
+  return (
+      <Table columns={columns} dataSource={props.showList} />
+    )
+}
+
+class Searching extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showList: data
+    }
+
+  }
+  onSearch = (value) => {
+    if(value === '' || value === null){
+      return
+    }
+    let res = data.filter(item=>item['Organisation Name'].indexOf(value)>=0);
+    this.setState({
+      showList:res
+    })
+    console.log(this.state.showList.length)
+  };
+  render() {
+    return (
+      <div className="game">
+        <div className="searchBox">
+          <Search
+            placeholder="input search text"
+            enterButton="Search"
+            size="large"
+            onSearch={this.onSearch}
+          />
+        </div>
+        <div className="listBox">
+          <Item showList={this.state.showList}/>
+
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default Searching
